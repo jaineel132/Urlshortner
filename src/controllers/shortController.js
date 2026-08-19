dd6b10bc81ec248c55faaa901bd8fcfbec450d42
jaineel132 <jaineeljjp@gmail.com>
2026-08-19 20:08:55 +0530

    refactor: centralize controller error handling

--- src/controllers/shortController.js
+++ src/controllers/shortController.js
@@ -1,52 +1,37 @@
 import {shortenURL,getOgURL,deleteShortURL} from "../services/urlService.js";
 
-async function shortenURLController(req, res) {
+async function shortenURLController(req, res,next) {
     const { original_url, custom_alias, expires_at } = req.body;
     try{
      const result = await shortenURL(original_url, custom_alias, expires_at);
             res.status(201).json({ short_url: process.env.BASE_URL+"/"+result.short_code ,expires_at: result.expires_at });
         }
     catch(error) {
-            console.error('Error shortening URL:', error);
-            if (error.statusCode === 409) {
-                res.status(409).json({ error: error.message });
-            } else {
-                res.status(500).json({ error: 'An error occurred while shortening the URL' });
-            }
+            next(error);
         }
 
 }
 
-async function getOriginalURLController(req, res) {
+async function getOriginalURLController(req, res,next) {
     const { shortcode } = req.params;
     try{
         const result = await getOgURL(shortcode);
         res.redirect(result.original_url);
     }
     catch(error) {
-        if(error.statusCode){
-            res.status(error.statusCode).json({ error: error.message });
-        }
-        else{
-            res.status(500).json({ error: 'An error occurred while retrieving the original URL' });
-        }
+        next(error);
     }
 }
 
 
-async function deleteShortURLController(req, res) {
+async function deleteShortURLController(req, res,next) {
     const {shortcode} = req.params
     try{
         const result = await deleteShortURL(shortcode);
         res.status(200).json({ message: 'Short URL deleted successfully' });
     }
     catch(error){
-        if(error.statusCode){
-            res.status(error.statusCode).json({ error: error.message });
-        }
-        else{
-            res.status(500).json({ error: 'An error occurred while deleting the short URL' });
-        }
+        next(error);
 }
 }
 
